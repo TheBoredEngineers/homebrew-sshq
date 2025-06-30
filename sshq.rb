08adfb7ee3332b2bcb03c42b75ecd99f93523703
Kshitij Kishore Shetty <118621124+kshitijshettyyy@users.noreply.github.com>
2025-06-30 11:45:35 +0530

    Update sshq.rb

--- sshq.rb
+++ sshq.rb
@@ -1,7 +1,7 @@
 class Sshq < Formula
   desc "SSH Simplified for Devs"
   homepage "https://github.com/kshitijshettyyy/sshq"
-  url "https://github.com/kshitijshettyyy/sshq/releases/download/v1.0.0/sshq"
+  url "https://github.com/kshitijshettyyy/sshq/releases/download/v1.0.1/sshq"
   sha256 "088461fa339b6edd7dcd1fe5ada71b40b24e85304d385651c5115ff94adc9289"
   license "MIT"
 
@@ -13,3 +13,4 @@ class Sshq < Formula
     system "#{bin}/sshq"
   end
 end
+
