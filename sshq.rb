6498ecd51785f98f19f5efae3c5a9b9d092297ca
Kshitij Kishore Shetty <118621124+kshitijshettyyy@users.noreply.github.com>
2025-11-26 17:41:13 +0530

    Update homepage and URL for sshq

--- sshq.rb
+++ sshq.rb
@@ -1,7 +1,7 @@
 class Sshq < Formula
   desc "SSH Simplified for Devs"
-  homepage "https://github.com/kshitijshettyyy/sshq"
-  url "https://github.com/kshitijshettyyy/sshq/releases/download/v1.0.3/sshq"
+  homepage "https://github.com/TheBoredEngineers/sshq"
+  url "https://github.com/TheBoredEngineers/sshq/releases/download/v1.0.3/sshq"
   sha256 "4b5b37c620b62076f072b1d5907676176f28ef8a6e485a6ac67411e91024e81c"
   license "MIT"
 
